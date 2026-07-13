ee4b5a0d584ecd2b7e0d122f93375501a15eef54
sanchit-195 <sanchitkumarjaiswal@gmail.com>
2026-07-13 14:08:28 +0530

    Refactor MongoDB connection code for improved readability

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,7 @@ import { DB_NAME } from '../constants.js';
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(
-            `${process.env.MONGODB_URL}/${DB_NAME}`
-        );
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         console.log(
             "MongoDB connected successfully! Host:",
             connectionInstance.connection.host
